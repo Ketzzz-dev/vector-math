811346809027e59606bcf60f48f52f0e573f29ec
Low-Key-750 <0hlowkey750@gmail.com>
2021-07-20 17:17:33 +0200

    changed export to default

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,24 +1,22 @@
-export declare namespace VectorMath {
-    class Vector2 {
-        readonly x: number;
-        readonly y: number;
-        static readonly ZERO: Vector2;
-        constructor(x: number, y: number);
-        get negative(): Vector2;
-        get magnitude(): number;
-        get normal(): Vector2;
-        toString(): string;
-    }
-    abstract class MathV {
-        static add(a: Vector2, b: Vector2): Vector2;
-        static subtract(a: Vector2, b: Vector2): Vector2;
-        static multiply(v: Vector2, s: number): Vector2;
-        static divide(v: Vector2, s: number): Vector2;
-        static equals(a: Vector2, b: Vector2): boolean;
-        static isNaN(v: Vector2): boolean;
-        static isFinite(v: Vector2): boolean;
-        static distance(a: Vector2, b: Vector2): number;
-        static dot(a: Vector2, b: Vector2): number;
-        static cross(a: Vector2, b: Vector2): number;
-    }
+export declare class Vector2 {
+    readonly x: number;
+    readonly y: number;
+    static readonly ZERO: Vector2;
+    constructor(x: number, y: number);
+    get negative(): Vector2;
+    get magnitude(): number;
+    get normal(): Vector2;
+    toString(): string;
+}
+export declare abstract class MathV {
+    static add(a: Vector2, b: Vector2): Vector2;
+    static subtract(a: Vector2, b: Vector2): Vector2;
+    static multiply(v: Vector2, s: number): Vector2;
+    static divide(v: Vector2, s: number): Vector2;
+    static equals(a: Vector2, b: Vector2): boolean;
+    static isNaN(v: Vector2): boolean;
+    static isFinite(v: Vector2): boolean;
+    static distance(a: Vector2, b: Vector2): number;
+    static dot(a: Vector2, b: Vector2): number;
+    static cross(a: Vector2, b: Vector2): number;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,76 +1,73 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VectorMath = void 0;
-var VectorMath;
-(function (VectorMath) {
-    var Vector2 = /** @class */ (function () {
-        function Vector2(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-        Object.defineProperty(Vector2.prototype, "negative", {
-            get: function () {
-                return new Vector2(-this.x, -this.y);
-            },
-            enumerable: false,
-            configurable: true
-        });
-        Object.defineProperty(Vector2.prototype, "magnitude", {
-            get: function () {
-                return Math.sqrt(this.x * this.x + this.y * this.y);
-            },
-            enumerable: false,
-            configurable: true
-        });
-        Object.defineProperty(Vector2.prototype, "normal", {
-            get: function () {
-                return MathV.divide(this, this.magnitude);
-            },
-            enumerable: false,
-            configurable: true
-        });
-        Vector2.prototype.toString = function () {
-            return "x: " + this.x + ", y: " + this.y;
-        };
-        Vector2.ZERO = new Vector2(0, 0);
-        return Vector2;
-    }());
-    VectorMath.Vector2 = Vector2;
-    var MathV = /** @class */ (function () {
-        function MathV() {
-        }
-        MathV.add = function (a, b) {
-            return new Vector2(a.x + b.x, a.y + b.y);
-        };
-        MathV.subtract = function (a, b) {
-            return new Vector2(a.x - b.x, a.y - b.y);
-        };
-        MathV.multiply = function (v, s) {
-            return new Vector2(v.x * s, v.y * s);
-        };
-        MathV.divide = function (v, s) {
-            return new Vector2(v.x / s, v.y / s);
-        };
-        MathV.equals = function (a, b) {
-            return a.x === b.x && a.y === b.y;
-        };
-        MathV.isNaN = function (v) {
-            return isNaN(v.x) || isNaN(v.y);
-        };
-        MathV.isFinite = function (v) {
-            return isFinite(v.x) && isFinite(v.y);
-        };
-        MathV.distance = function (a, b) {
-            return MathV.subtract(a, b).magnitude;
-        };
-        MathV.dot = function (a, b) {
-            return a.x * b.x + a.y * b.y;
-        };
-        MathV.cross = function (a, b) {
-            return a.x * b.y + a.y * b.x;
-        };
-        return MathV;
-    }());
-    VectorMath.MathV = MathV;
-})(VectorMath = exports.VectorMath || (exports.VectorMath = {}));
+exports.MathV = exports.Vector2 = void 0;
+var Vector2 = /** @class */ (function () {
+    function Vector2(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Object.defineProperty(Vector2.prototype, "negative", {
+        get: function () {
+            return new Vector2(-this.x, -this.y);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Vector2.prototype, "magnitude", {
+        get: function () {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Vector2.prototype, "normal", {
+        get: function () {
+            return MathV.divide(this, this.magnitude);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Vector2.prototype.toString = function () {
+        return "x: " + this.x + ", y: " + this.y;
+    };
+    Vector2.ZERO = new Vector2(0, 0);
+    return Vector2;
+}());
+exports.Vector2 = Vector2;
+var MathV = /** @class */ (function () {
+    function MathV() {
+    }
+    MathV.add = function (a, b) {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    };
+    MathV.subtract = function (a, b) {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    };
+    MathV.multiply = function (v, s) {
+        return new Vector2(v.x * s, v.y * s);
+    };
+    MathV.divide = function (v, s) {
+        return new Vector2(v.x / s, v.y / s);
+    };
+    MathV.equals = function (a, b) {
+        return a.x === b.x && a.y === b.y;
+    };
+    MathV.isNaN = function (v) {
+        return isNaN(v.x) || isNaN(v.y);
+    };
+    MathV.isFinite = function (v) {
+        return isFinite(v.x) && isFinite(v.y);
+    };
+    MathV.distance = function (a, b) {
+        return MathV.subtract(a, b).magnitude;
+    };
+    MathV.dot = function (a, b) {
+        return a.x * b.x + a.y * b.y;
+    };
+    MathV.cross = function (a, b) {
+        return a.x * b.y + a.y * b.x;
+    };
+    return MathV;
+}());
+exports.MathV = MathV;
 //# sourceMappingURL=index.js.map
